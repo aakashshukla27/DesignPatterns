b96e61f254868fda825c08528bbc4321f939dcfb
Aakash Shukla <shukla.aa@northeastern.edu>
2021-09-27 01:54:02 -0400

    Added SimUDuck

---
+++ SimUDuck/Duck.cs
@@ -0,0 +1,37 @@
+﻿using SimUDuck.Fly;
+using SimUDuck.Sound;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck
+{
+    public abstract class Duck
+    {
+        IFlyBehavior flyBehavior;
+        IQuackBehavior quackBehavior;
+        public Duck()
+        {
+
+        }
+        public abstract void Display();
+
+        public void PerformFly()
+        {
+            flyBehavior.Fly();
+        }
+        
+        public void PerformQuack()
+        {
+            quackBehavior.Quack();
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("All ducks can swim");
+        }
+
+    }
+}
---
+++ SimUDuck/Sound/MuteQuack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimUDuck.Sound
+{
+    class MuteQuack : IQuackBehavior
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Silence");
+        }
+    }
+}
